768a031f7507148bbb5be199844291b79b9d3d34
Gabriel <gabriel.n64@hotmail.com>
2018-05-20 00:03:15 -0300

    corrigido todos os bugs (falta fazer formulário voltar um passo atrás)

--- js/formPadraoController.js
+++ js/formPadraoController.js
@@ -7,6 +7,7 @@
 var arrFormList = [];
 var produtoSelecionado;
 var app = angular.module("myApp", []);
+var respostaEmail = "";
 
 app.controller("meuController", function ($scope, $http, $compile) {
 
@@ -26,17 +27,24 @@ function getFormProduct($http, $scope, $compile, produtoSelecionado){
     $http.get('json/'+produtoSelecionado).then(
             function (response) {
 
-                var perguntaAtual = 0, varTroca, jsonList = [], respostaEmail = "";
+                var perguntaAtual = 0, varTroca, jsonList = [];
 
                 jsonList = response.data.perguntas;
 
                 if (perguntaAtual == 0) {
                     if (!jsonList[0].temResposta) {
-                console.log(perguntaAtual);
 
-                        console.log(jsonList[0].html);
-                        angular.element(document.getElementById('form_2'))
-                                .append($compile(jsonList[0].html)($scope));
+
+                        respostaEmail = respostaEmail + "<br>" + "<strong>" + jsonList[perguntaAtual].pergunta + "</strong>";
+                        var inputs = jsonList[perguntaAtual].respostas;
+                                         var resposta;
+
+                        for (var i = 0; i <= inputs.length; i++) {
+                            var id = "#"+inputs;
+                                          resposta = $(id).val();
+                            espostaEmail = respostaEmail +  "<br>" + resposta + "</br>";
+                        }
+
                     }else{
                     
                         respostaEmail = respostaEmail + "<br>" + "<strong>" + jsonList[perguntaAtual].pergunta + "</strong>";
@@ -66,16 +74,23 @@ function getFormProduct($http, $scope, $compile, produtoSelecionado){
                 }
 
                 $scope.nextFormProduct = function () {
-                    console.log(perguntaAtual);
-                    console.log(jsonList.length-1);
+                                     
                     if (perguntaAtual >= jsonList.length-1) {
                         //getFormProduct($http, $scope, $compile, produtoSelecionado);
                     }else{
                         
                         if (!jsonList[perguntaAtual].temResposta) {
 
+                        respostaEmail = respostaEmail + "<br>" + "<strong>" + jsonList[perguntaAtual].pergunta + "</strong>";
+                        var inputs = jsonList[perguntaAtual].respostas;
+                                         var resposta;
 
-                        respostaEmail = respostaEmail + "<br>" + "<strong>" + jsonList[perguntaAtual].pergunta + "</strong>" + "<br>" + $scope.resNomeEmpresa + "<br>";
+                        for (var i = 0; i < inputs.length; i++) {
+                            var id = "#"+inputs;
+                            resposta = $(id).val();
+                            respostaEmail = respostaEmail +  "<br>" + resposta + "</br>";
+                        }
+
 
                         $("#right-panel").fadeIn();
                         $("#right-panel").html(respostaEmail);
@@ -86,23 +101,39 @@ function getFormProduct($http, $scope, $compile, produtoSelecionado){
 
                         angular.element(document.getElementById('form_2'))
                                 .append($compile(jsonList[perguntaAtual].html)($scope));
+                            
 
                     }else{
                     
                         respostaEmail = respostaEmail + "<br>" + "<strong>" + jsonList[perguntaAtual].pergunta + "</strong>";
 
                         var listaRespostas = jsonList[perguntaAtual].respostas, x;
+
+                
+                        
+                        
                         
                         for (x = 0; x < listaRespostas.length; x++){
 
                             var checked, id = '#'+listaRespostas[x];
 
                             if($('#'+listaRespostas[x]).is(':checked')){
+                                
                                 respostaEmail = respostaEmail +  "<br>" + listaRespostas[x] + "</br>";
                             }
 
                         }
-                           
+                        
+                        
+
+                            if($( "input[name='radio']" ).is(":checked")){
+                                checked = $( "input[name='radio']:checked" ).val(); 
+                                respostaEmail = respostaEmail +  "<br>" +  checked + "</br>";
+                            }
+
+                       
+  
+                       
                         
                         $("#right-panel").fadeIn();
                         $("#right-panel").html(respostaEmail);
@@ -126,30 +157,32 @@ function getFormPardao($http, $scope, $compile){
      $http.get('json/form-padrao.json').then(
             function (response) {
 
-                var perguntaAtual = 0, varTroca, jsonList = [], respostaEmail = "";
+                var perguntaAtual = 0, varTroca, jsonList = [];
                 jsonList = response.data.perguntas;
 
-                console.log(jsonList);
-
-                if (perguntaAtual == 0) {
+                         if (perguntaAtual == 0) {
                     if (!jsonList[0].temResposta) {
 
-                        angular.element(document.getElementById('form_2'))
-                                .append($compile(jsonList[0].html)($scope));
+                        
+
+                        angular.element(document.getElementById('form_2')).append($compile(jsonList[0].html)($scope));
+
                     }
                 }
 
                 $scope.next = function () {
                     
                     if (perguntaAtual >= jsonList.length-1) {
-                        
-                        getFormProduct($http, $scope, $compile, produtoSelecionado);
-                    }else{
-                        console.log(perguntaAtual);
-                        if (!jsonList[perguntaAtual].temResposta) {
 
+                        respostaEmail = respostaEmail + "<br>" + "<strong>" + jsonList[perguntaAtual].pergunta + "</strong>";
+                        var inputs = jsonList[perguntaAtual].respostas;
+                                         var resposta;
 
-                        respostaEmail = respostaEmail + "<br>" + "<strong>" + jsonList[perguntaAtual].pergunta + "</strong>" + "<br>" + $scope.resNomeEmpresa + "<br>";
+                        for (var i = 0; i < inputs.length; i++) {
+                            var id = "#"+inputs;
+                            resposta = $(id).val();
+                                                respostaEmail = respostaEmail +  "<br>" + resposta + "</br>";
+                        }
 
                         $("#right-panel").fadeIn();
                         $("#right-panel").html(respostaEmail);
@@ -157,15 +190,39 @@ function getFormPardao($http, $scope, $compile){
                         perguntaAtual += 1;
 
                         angular.element(document.getElementById('form_2')).empty();
+                        
+                        getFormProduct($http, $scope, $compile, produtoSelecionado);
+                    }else{
 
-                        angular.element(document.getElementById('form_2'))
-                                .append($compile(jsonList[perguntaAtual].html)($scope));
+                                             if (!jsonList[perguntaAtual].temResposta) {
+
+                        respostaEmail = respostaEmail + "<br>" + "<strong>" + jsonList[perguntaAtual].pergunta + "</strong>";
+                        var inputs = jsonList[perguntaAtual].respostas;
+                                         var resposta;
+
+                        for (var i = 0; i < inputs.length; i++) {
+                            var id = "#"+inputs;
+                            resposta = $(id).val();
+                                                respostaEmail = respostaEmail +  "<br>" + resposta + "</br>";
+                        }
+
+
+                        $("#right-panel").fadeIn();
+                        $("#right-panel").html(respostaEmail);
+
+                        perguntaAtual += 1;
+
+                        angular.element(document.getElementById('form_2')).empty();
+                        angular.element(document.getElementById('form_2')).append($compile(jsonList[perguntaAtual].html)($scope));
+
 
                     }else{
                     
                         respostaEmail = respostaEmail + "<br>" + "<strong>" + jsonList[perguntaAtual].pergunta + "</strong>";
 
                         var listaRespostas = jsonList[perguntaAtual].respostas, x;
+
+
                         
                         for (x = 0; x < listaRespostas.length; x++){
 
@@ -176,7 +233,8 @@ function getFormPardao($http, $scope, $compile){
                             }
 
                         }
-                           
+
+                       
                         
                         $("#right-panel").fadeIn();
                         $("#right-panel").html(respostaEmail);
@@ -186,7 +244,7 @@ function getFormPardao($http, $scope, $compile){
                         angular.element(document.getElementById('form_2')).empty();
                         angular.element(document.getElementById('form_2')).append($compile(jsonList[perguntaAtual].html)($scope));
                         
-                    }
+                        }
                     }
 
                 };
@@ -204,13 +262,13 @@ function preparaPerguntaFormPadrao(listaDePerguntas) {
     var firstQuestion = 0;
 
     for (x = 0; x < listaDePerguntas.length; x++) {
-        // console.log(listaDePerguntas[x].html);
+        // .html);
         if (listaDePerguntas[x].temResposta) {
 
             var respostas = listaDePerguntas[x].respostas;
 
             for (var y = 0; y < respostas.length; y++) {
-                //console.log("  " + y + " . " + respostas[y]);
+                //respostas[y]);
             }
         }
     }}
